a507f658a93d7b3ad631ed95792c101e1594fbec
Sumit Ranjan <sumitranjan.ind@gmail.com>
2023-01-01 23:32:21 +0530

    How to Start read project.

---
+++ 01-Inception/app.js
@@ -0,0 +1,16 @@
+const heading1 = React.createElement("h1", { id: "title1" }, "Heading 1");
+
+const heading2 = React.createElement("h2", { id: "title2" }, "Heading 2");
+
+const container = React.createElement("div", { id: "container" }, [
+  heading1,
+  heading2,
+]);
+console.log(container);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//passing a react element inside the root
+
+//async defer
+root.render(container);
